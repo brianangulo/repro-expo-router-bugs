0117f0deb6aed762100b19f9e7cb91c3d3cf5b1d
Brian Angulo <brianangulo96@gmail.com>
2023-11-08 14:49:21 -0600

    Expo Router relative import repro

--- app/(foo)/_layout.tsx
+++ app/(foo)/_layout.tsx
@@ -1,20 +1,10 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Layout() {
   return (
-    <View
-      style={{
-        flex: 1,
-        margin: 20,
-        borderColor: "darkred",
-        borderWidth: 2,
-      }}
-    >
-      <Text style={{ backgroundColor: "darkred", color: "white" }}>
-        (foo)/_layout.tsx
-      </Text>
-      <Slot />
-    </View>
+    <Stack>
+      <Stack.Screen name="/test/other" />
+    </Stack>
   );
 }
